fb75b7d30e0c47950b1ca9ef5d4306df6735eff1
unknown <rostykvoznyak@gmail.com>
2020-10-22 20:58:30 +0300

    Lesson1Task4

---
+++ Lesson1Task4/Lesson1Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson1Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            float k = 1.0f / 2;
+            double i = 1.0 / 2;
+            Console.WriteLine(k);
+            Console.WriteLine(i);
+        }
+    }
+}
